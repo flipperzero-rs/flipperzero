ba613eb69f669b3d0770ac69c80acd7de436f084
David Coles <coles.david@gmail.com>
2022-10-01 16:48:55 -0700

    Implement print! and println!
    The `println!` macro uses `CR`+`NL` instead of just `NL`, because
    the Flipper CLI doesn't appear to automatically `CR` on `NL` like most
    POSIX systems.

--- crates/flipperzero/src/lib.rs
+++ crates/flipperzero/src/lib.rs
@@ -4,3 +4,17 @@
 
 pub mod furi;
 pub mod panic_handler;
+
+#[macro_export]
+macro_rules! print {
+    ($($args:tt)*) => {
+        core::write!($crate::furi::io::Stdout, $($args)*).unwrap()
+    };
+}
+
+#[macro_export]
+macro_rules! println {
+    ($pat:expr, $($args:tt)*) => {
+        core::write!($crate::furi::io::Stdout, core::concat!($pat, "\r\n"), $($args)*).unwrap()
+    };
+}
