47ecb9e9636ae518da78b93e376836c9aa05cbd5
David Coles <coles.david@gmail.com>
2022-09-30 21:51:10 -0700

    Add sys bindings for Furi memory management API

---
+++ crates/sys/src/furi/memmgr.rs
@@ -0,0 +1,15 @@
+//! Furi memory management API.
+
+use core::ffi::c_void;
+
+extern "C" {
+    #[link_name="memmgr_get_free_heap"]
+    pub fn get_free_heap() -> usize;
+    #[link_name="memmgr_get_total_heap"]
+    pub fn get_total_heap() -> usize;
+    #[link_name="memmgr_get_minimum_free_heap"]
+    pub fn get_minimum_free_heap() -> usize;
+
+    pub fn aligned_malloc(size: usize, alignment: usize) -> *mut c_void;
+    pub fn aligned_free(p: *mut c_void);
+}
