3c9a80550ee5dc4292a5b6a94456446ec6839f69
David Coles <coles.david@gmail.com>
2022-10-15 15:27:13 -0700

    Fix println! macro
    Previously the macro would fail on a single string argument or
    if the `core::fmt::Write` trait wasn't in scope.

--- crates/flipperzero/src/lib.rs
+++ crates/flipperzero/src/lib.rs
@@ -8,13 +8,21 @@ pub mod panic_handler;
 #[macro_export]
 macro_rules! print {
     ($($args:tt)*) => {
-        core::write!($crate::furi::io::Stdout, $($args)*).unwrap()
+        core::fmt::write(&mut $crate::furi::io::Stdout, core::format_args!($($args)*)).unwrap()
     };
 }
 
 #[macro_export]
 macro_rules! println {
     ($pat:expr, $($args:tt)*) => {
-        core::write!($crate::furi::io::Stdout, core::concat!($pat, "\r\n"), $($args)*).unwrap()
+        {
+            core::fmt::write(&mut $crate::furi::io::Stdout, core::format_args!(core::concat!($pat, "\r\n"), $($args)*)).unwrap();
+        }
+    };
+
+    ($msg:expr) => {
+        {
+            core::fmt::write(&mut $crate::furi::io::Stdout, core::format_args!(core::concat!($msg, "\r\n"))).unwrap();
+        }
     };
 }
