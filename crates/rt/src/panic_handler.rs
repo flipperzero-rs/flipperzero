c497cd5609c3d905181001188616d956466af766
David Coles <coles.david@gmail.com>
2022-10-22 19:34:29 -0700

    Rewrite panic handler without using fmt
    Using `core::fmt` can easily pull in 10+ KiB of formatting code for
    something as simple as formatting a string. Instead use `printf`.

--- crates/rt/src/panic_handler.rs
+++ crates/rt/src/panic_handler.rs
@@ -1,51 +1,42 @@
 //! Panic handler for Furi applications.
 //! This will print the panic info to stdout and then trigger a crash.
 
-use core::ffi::{CStr, c_char};
-use core::fmt::Write;
+use core::ffi::c_char;
 use core::panic::PanicInfo;
 use core::str;
 
 use flipperzero_sys as sys;
-
-/// Minimal [`Stdout`] implementation.
-struct Stdout;
-
-impl Write for Stdout {
-    fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        let len = s.len();
-        unsafe {
-            if sys::furi_thread_stdout_write(s.as_ptr() as *mut c_char, len) != len {
-                return Err(core::fmt::Error);
-            }
-        }
-
-        Ok(())
-    }
-}
+use sys::c_string;
 
 #[panic_handler]
 pub fn panic(panic_info: &PanicInfo<'_>) -> ! {
-    let thread_name = unsafe {
-        let thread_id = sys::furi_thread_get_current_id();
-        let thread_name = sys::furi_thread_get_name(thread_id);
-
-        if thread_name.is_null() {
-            "<unknown>"
-        } else {
-            str::from_utf8_unchecked(CStr::from_ptr(thread_name).to_bytes())
-        }
-    };
-
     // Format: "thread: 'App Name' paniced at 'panic!', panic.rs:5"
-    let _ = write!(
-        &mut Stdout,
-        "\x1b[0;31mthread '{thread_name}' {panic_info}\x1b[0m\r\n"
-    );
-
+    // Note: Don't use `format!` as it pulls in 10 KiB+ of formatting code.
     unsafe {
+        let thread_id = sys::furi_thread_get_current_id();
+        let thread_name = if !thread_id.is_null() {
+            sys::furi_thread_get_name(thread_id)
+        } else {
+            c_string!("unknown")
+        };
+
+        sys::__wrap_printf(c_string!("\x1b[0;31mthread: '%s' paniced"), thread_name);
+
+        if let Some(s) = panic_info.payload().downcast_ref::<&str>() {
+            sys::__wrap_printf(c_string!(" at '%*s'"), s.len(), s.as_ptr() as *const c_char);
+        }
+
+        if let Some(location) = panic_info.location() {
+            let file = location.file();
+            let line = location.line();
+
+            sys::__wrap_printf(c_string!(", %*s:%u"), file.len(), file.as_ptr() as *const c_char, line);
+        }
+
+        sys::__wrap_printf(c_string!("\x1b[0m\r\n"));
         sys::furi_thread_stdout_flush();
         sys::furi_thread_yield(); // Allow console to flush
+
         sys::crash!("Rust panic")
     }
 }
