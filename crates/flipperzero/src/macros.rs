1c62ae030fb3f102ae99b00dac40ce1638b86c2c
Petr Portnov <gh@progrm-jarvis.ru>
2022-12-30 22:06:59 +0300

    feat: allow trailing comas in function-like macro calls

--- crates/flipperzero/src/macros.rs
+++ crates/flipperzero/src/macros.rs
@@ -2,22 +2,22 @@
 
 #[macro_export]
 macro_rules! print {
-    ($pat:expr, $($args:tt)*) => {{
+    ($pat:expr, $($args:tt)* $(,)?) => {{
         $crate::furi::io::_print(core::format_args!($pat, $($args)*));
     }};
 
-    ($msg:expr) => {{
+    ($msg:expr $(,)?) => {{
         $crate::furi::io::_write_str($msg);
     }};
 }
 
 #[macro_export]
 macro_rules! println {
-    ($pat:expr, $($args:tt)*) => {{
+    ($pat:expr, $($args:tt)* $(,)?) => {{
         $crate::furi::io::_print(core::format_args!(concat!($pat, "\r\n"), $($args)*));
     }};
 
-    ($msg:expr) => {{
+    ($msg:expr $(,)?) => {{
         $crate::furi::io::_write_str(concat!($msg, "\r\n"));
     }};
 }
