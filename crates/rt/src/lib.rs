4211c9b82eb11ed7d27eb0e6e79fb5513215ac1c
Jack Grigg <thestr4d@gmail.com>
2023-03-03 22:24:34 +0000

    Implement `std::thread`-like wrappers around Furi Thread APIs
    Bumps pinned nightly to one where `Arc::into_inner` is stabilized.

--- crates/rt/src/lib.rs
+++ crates/rt/src/lib.rs
@@ -1,5 +1,5 @@
 //! Rust Runtime for the Flipper Zero.
-//! 
+//!
 //! This must be build with `-Z no-unique-section-names` to ensure that this module
 //! is linked directly into the `.text` section.
 
@@ -28,10 +28,54 @@ macro_rules! entry {
         // lld seems not to automatically rename `.rel.text.main` properly.
         #[export_name = "main"]
         pub unsafe fn __main(args: *mut u8) -> i32 {
+            use core::{ffi::CStr, mem, ptr, slice};
+            use flipperzero_sys as sys;
+
             // type check the entry function
             let f: fn(*mut u8) -> i32 = $path;
 
-            f(args)
+            let ret = f(args);
+
+            // Wait for threads with the same app ID to finish. We assume there are not
+            // more than 30 threads running in the background outside this app, so we will
+            // observe at least one of any thread the app might have left running.
+            unsafe {
+                sys::furi_log_print_format(
+                    sys::FuriLogLevel_FuriLogLevelDebug,
+                    sys::c_string!("flipperzero-rt"),
+                    sys::c_string!("Waiting for FAP threads to complete..."),
+                );
+            }
+            const MAX_THREADS: usize = 32;
+            let cur_thread_id = unsafe { sys::furi_thread_get_current_id() };
+            let app_id = unsafe { CStr::from_ptr(sys::furi_thread_get_appid(cur_thread_id)) };
+            let mut thread_ids: [sys::FuriThreadId; MAX_THREADS] = [ptr::null_mut(); MAX_THREADS];
+            loop {
+                let thread_count = unsafe {
+                    sys::furi_thread_enumerate(thread_ids.as_mut_ptr(), MAX_THREADS as u32)
+                } as usize;
+
+                let running = thread_ids[..thread_count].into_iter().any(|&thread_id| {
+                    let thread_app_id =
+                        unsafe { CStr::from_ptr(sys::furi_thread_get_appid(thread_id)) };
+                    thread_id != cur_thread_id && thread_app_id == app_id
+                });
+
+                if running {
+                    unsafe { sys::furi_delay_ms(10) };
+                } else {
+                    break;
+                }
+            }
+            unsafe {
+                sys::furi_log_print_format(
+                    sys::FuriLogLevel_FuriLogLevelDebug,
+                    sys::c_string!("flipperzero-rt"),
+                    sys::c_string!("All threads completed, exiting FAP"),
+                );
+            }
+
+            ret
         }
-    }
+    };
 }
