56a02564ef08a4bd72d60275782e611293d836e1
Jack Grigg <thestr4d@gmail.com>
2023-03-19 00:47:37 +0000

    Implement wrapper around SHA-256 primitive
    We use the `digest` crate traits to provide the safe API, for
    interoperability with the RustCrypto ecosystem.

---
+++ crates/flipperzero/src/toolbox/mod.rs
@@ -0,0 +1,15 @@
+//! Various tools provided by the Flipper Zero SDK.
+//!
+//! Some of these tools have common pure-Rust equivalents in the crate ecosystem; these
+//! are documented on each tool. The types provided here enable application developers to
+//! choose their trade-offs:
+//!
+//! - Using a type in this module means re-using the implementation embedded into the
+//!   Flipper Zero firmware. This reduces the size of the application binary, but requires
+//!   calls into the Flipper Zero SDK that the Rust compiler cannot optimize away.
+//!
+//! - Using an equivalent pure-Rust type enables the Rust compiler to optimize the
+//!   application more effectively, at the cost of larger binary size.
+
+pub(crate) mod sha256;
+pub use sha256::Sha256;
