b12c3cd6afe6d880c62891810568ba024d01842c
Bruno Wieczorek <bruno.wieczorek@gmail.com>
2023-03-29 19:38:13 +0200

    Support MacOS with M1 for generate-bindings.rs

--- tools/src/bin/generate-bindings.rs
+++ tools/src/bin/generate-bindings.rs
@@ -20,6 +20,8 @@ const TOOLCHAIN: &str = "../../../toolchain/x86_64-windows/arm-none-eabi/include
 const TOOLCHAIN: &str = "../../../toolchain/i686-linux/arm-none-eabi/include";
 #[cfg(all(unix, target_arch = "x86_64"))]
 const TOOLCHAIN: &str = "../../../toolchain/x86_64-linux/arm-none-eabi/include";
+#[cfg(all(target_os="macos", target_arch = "aarch64"))]
+const TOOLCHAIN: &str = "../../../toolchain/x86_64-darwin/arm-none-eabi/include";
 const VISIBILITY_PUBLIC: &str = "+";
 
 #[derive(Debug)]
