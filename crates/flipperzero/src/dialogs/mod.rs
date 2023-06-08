48a7128ec0203c34a28d532dbe911d52e627c35a
Michael Mogenson <michael.mogenson@gmail.com>
2023-06-07 20:35:47 -0400

    Dialogs app file browser wrapper
    Add a safe Rust wrapper for the dialog file browser methods and the
    DialogsFileBrowserOptions type. Rename the DialogsApp::show() method
    to show_message() and create new show_file_browser() method. Add a
    getter method to FuriString to provide access to the inner
    sys::FuriString via a raw pointer because a sys::FuriString is needed by
    a few of the Furi C API functions. Extend the storage example to open a
    file browser and have the user select the file that was just written to
    the file system.
    
    Note: The Furi C struct is named DialogsFileBrowserOptions, but all
    other dialog types and functions are singular instead of plural. The
    Rust type is named DialogFileBrowserOptions.
    
    The DialogFileBrowserOptions type uses setter methods that return Self
    so they can be chained together in the builder pattern. The
    DialogFileBrowserOptions includes a Rust unit type phantom data member.
    The type or size does not really matter, but PhantomData does require a
    sized type. We just need one struct member to carry the lifetime
    parameter so that CStr and Icon references are not dropped until the
    struct goes out of scope.

--- crates/flipperzero/src/dialogs/mod.rs
+++ crates/flipperzero/src/dialogs/mod.rs
@@ -3,13 +3,14 @@
 #[cfg(feature = "alloc")]
 use alloc::ffi::CString;
 
-use core::ffi::{c_char, CStr};
+use core::ffi::{c_char, c_void, CStr};
 use core::marker::PhantomData;
 use core::ptr::{self, NonNull};
 
 use flipperzero_sys as sys;
-use flipperzero_sys::furi::UnsafeRecord;
+use sys::{c_string, furi::UnsafeRecord};
 
+use crate::furi::string::FuriString;
 use crate::gui::canvas::Align;
 
 const RECORD_DIALOGS: *const c_char = sys::c_string!("dialogs");
@@ -26,6 +27,12 @@ pub struct DialogsApp {
 pub struct DialogMessage<'a> {
     data: NonNull<sys::DialogMessage>,
     _phantom: PhantomData<&'a CStr>,
+}
+
+/// A dialog file browser options.
+pub struct DialogFileBrowserOptions<'a> {
+    data: sys::DialogsFileBrowserOptions,
+    _phantom: PhantomData<&'a ()>,
 }
 
 /// Button pressed on a dialog.
@@ -45,11 +52,37 @@ impl DialogsApp {
     }
 
     /// Displays a message.
-    pub fn show(&mut self, message: &DialogMessage) -> DialogMessageButton {
+    pub fn show_message(&mut self, message: &DialogMessage) -> DialogMessageButton {
         let button_sys =
             unsafe { sys::dialog_message_show(self.data.as_ptr(), message.data.as_ptr()) };
 
         DialogMessageButton::from_sys(button_sys).expect("Invalid button")
+    }
+
+    /// Displays a file browser.
+    ///  - path is a optional preselected file path
+    ///  - options are optional file browser options
+    pub fn show_file_browser(
+        &mut self,
+        path: Option<&mut FuriString>,
+        options: Option<&DialogFileBrowserOptions>,
+    ) -> Option<FuriString> {
+        let mut result_path = FuriString::new();
+        // path will be unmodified but needs to be a valid FuriString.
+        // We can reuse the empty result_path if path is not provided.
+        let path = path.unwrap_or(&mut result_path).as_mut_ptr();
+        let options = options
+            .map(|opts| &opts.data as *const sys::DialogsFileBrowserOptions)
+            .unwrap_or(ptr::null());
+        unsafe {
+            sys::dialog_file_browser_show(
+                self.data.as_ptr(),
+                result_path.as_mut_ptr(),
+                path,
+                options,
+            )
+        }
+        .then_some(result_path)
     }
 }
 
@@ -164,6 +197,73 @@ impl DialogMessageButton {
     }
 }
 
+impl<'a> DialogFileBrowserOptions<'a> {
+    /// Creates a new dialog file browser options and initializes to default values.
+    pub fn new() -> Self {
+        Self {
+            // default values from sys::dialog_file_browser_set_basic_options()
+            data: sys::DialogsFileBrowserOptions {
+                extension: c_string!("*"),
+                base_path: ptr::null(),
+                skip_assets: true,
+                hide_dot_files: false,
+                icon: ptr::null(),
+                hide_ext: true,
+                item_loader_callback: None,
+                item_loader_context: ptr::null_mut(),
+            },
+            _phantom: PhantomData,
+        }
+    }
+
+    /// Set file extension to be offered for selection.
+    pub fn set_extension(mut self, extension: &'a CStr) -> Self {
+        self.data.extension = extension.as_ptr();
+        self
+    }
+
+    /// Set root folder path for navigation with back key.
+    pub fn set_base_path(mut self, base_path: &'a CStr) -> Self {
+        self.data.base_path = base_path.as_ptr();
+        self
+    }
+
+    /// Set file icon.
+    pub fn set_icon(mut self, icon: &'a sys::Icon) -> Self {
+        self.data.icon = icon as *const sys::Icon;
+        self
+    }
+
+    /// Do not show assets folder if true.
+    pub fn set_skip_assets(mut self, skip_assets: bool) -> Self {
+        self.data.skip_assets = skip_assets;
+        self
+    }
+
+    /// Hide dot files if true.
+    pub fn set_hide_dot_files(mut self, hide_dot_files: bool) -> Self {
+        self.data.hide_dot_files = hide_dot_files;
+        self
+    }
+
+    /// Hide extensions for files if true.
+    pub fn set_hide_ext(mut self, hide_ext: bool) -> Self {
+        self.data.hide_ext = hide_ext;
+        self
+    }
+
+    /// Set callback function and context for providing custom icon and entry name.
+    pub fn set_item_loader_callback(
+        mut self,
+        callback: sys::FileBrowserLoadItemCallback,
+        context: *mut c_void,
+    ) -> Self {
+        self.data.item_loader_callback = callback;
+        self.data.item_loader_context = context;
+        self
+    }
+}
+
 /// Displays a simple dialog.
 #[cfg(feature = "alloc")]
 pub fn alert(text: &str) {
@@ -175,5 +275,5 @@ pub fn alert(text: &str) {
     message.set_text(&text, 0, 0, Align::Left, Align::Top);
     message.set_buttons(None, Some(BUTTON_OK), None);
 
-    dialogs.show(&message);
+    dialogs.show_message(&message);
 }
