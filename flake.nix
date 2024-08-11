072ddd16ed96a4eb68ea22c7ca8853b039daf610
Petr Portnov <pportnov@ozon.ru>
2024-08-11 17:18:51 +0300

    build(nix): configure build environment via flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Rust on the Flipper Zero";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./crates/rust-toolchain.toml;
+      in {
+        devShells.default = pkgs.mkShell { nativeBuildInputs = [ rust pkgs.python3]; };
+      });
+}
