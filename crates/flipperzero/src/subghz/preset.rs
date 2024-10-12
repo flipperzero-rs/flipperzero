53ae86ecfc0ddc17d51f944a687e6acef3efca86
Erik Fong <quantumerikfong@gmail.com>
2024-10-11 20:05:36 -0400

    Removed commented out "Preset Builder" code so it can be developed in a separate branch

--- crates/flipperzero/src/subghz/preset.rs
+++ crates/flipperzero/src/subghz/preset.rs
@@ -1,5 +1,3 @@
-// #[cfg(feature = "alloc")]
-// use alloc::vec::Vec;
 use flipperzero_sys::{
     FuriHalSubGhzPreset, FuriHalSubGhzPreset_FuriHalSubGhzPreset2FSKDev238Async,
     FuriHalSubGhzPreset_FuriHalSubGhzPreset2FSKDev476Async,
@@ -11,7 +9,6 @@ use flipperzero_sys::{
     FuriHalSubGhzPreset_FuriHalSubGhzPresetOok650Async,
 };
 
-// #[cfg(feature = "alloc")]
 #[derive(Clone, Debug)]
 pub enum SubGhzPreset<'a> {
     /// default configuration
@@ -55,23 +52,6 @@ impl<'a> SubGhzPreset<'a> {
 pub struct CustomSubGhzPreset<'a>(&'a [u8]);
 
 impl<'a> CustomSubGhzPreset<'a> {
-    // #[cfg(feature = "alloc")]
-    // pub fn builder() -> CustomSubGhzPresetBuilder {
-    //     CustomSubGhzPresetBuilder {
-    //         data: Vec::new(),
-    //         pa_table: [0; 8],
-    //     }
-    // }
-
-    // #[cfg(feature = "alloc")]
-    // /// TODO: best way to describe the capacity variable?
-    // pub fn builder_with_capacity(capacity: usize) -> CustomSubGhzPresetBuilder {
-    //     CustomSubGhzPresetBuilder {
-    //         data: Vec::with_capacity(capacity + 8),
-    //         pa_table: [0; 8],
-    //     }
-    // }
-
     /// # Safety
     /// Read the manual for the CC1101 chip and review the [`furi_hal_subghz_load_custom_preset()`](https://github.com/flipperdevices/flipperzero-firmware/blob/0eaad8bf64f01a6f932647a9cda5475dd9ea1524/targets/f7/furi_hal/furi_hal_subghz.c#L162) function provided by the flipper API
     pub unsafe fn from_raw(raw_config: &[u8]) -> CustomSubGhzPreset {
@@ -82,20 +62,3 @@ impl<'a> CustomSubGhzPreset<'a> {
         self.0.as_ptr()
     }
 }
-
-// #[cfg(feature = "alloc")]
-// pub struct CustomSubGhzPresetBuilder {
-//     data: Vec<u8>,
-//     pa_table: [u8; 8],
-// }
-
-// impl CustomSubGhzPresetBuilder {
-//     pub fn build(mut self) -> CustomSubGhzPreset {
-//         // Null Terminate the register/data pairs.
-//         self.data.extend_from_slice(&[0, 0]);
-
-//         self.data.extend_from_slice(&self.pa_table);
-
-//         CustomSubGhzPreset(self.data)
-//     }
-// }
