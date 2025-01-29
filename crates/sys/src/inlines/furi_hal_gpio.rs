a6145e40a3f6df0b66f70b17b0b811e1cac3f6dc
Lofty Inclination <alastaircodd@hotmail.co.uk>
2025-01-29 21:00:04 +0000

    Address `unsafe_block_in_unsafe_fn` in safe wrappers

--- crates/sys/src/inlines/furi_hal_gpio.rs
+++ crates/sys/src/inlines/furi_hal_gpio.rs
@@ -16,10 +16,11 @@ pub const GPIO_NUMBER: usize = 16;
 /// `gpio` must be non-null, and the memory it points to must be initialized.
 #[inline]
 pub unsafe extern "C" fn furi_hal_gpio_write(gpio: *const sys::GpioPin, state: bool) {
-    let port = (*gpio).port;
-    let pin = (*gpio).pin;
+    let gpio = unsafe { *gpio };
+    let port = gpio.port;
+    let pin = gpio.pin;
 
-    furi_hal_gpio_write_port_pin(port, pin, state)
+    unsafe { furi_hal_gpio_write_port_pin(port, pin, state) }
 }
 
 /// GPIO write pin.
@@ -34,10 +35,12 @@ pub unsafe extern "C" fn furi_hal_gpio_write_port_pin(
     state: bool,
 ) {
     // writing to BSSR is an atomic operation
-    core::ptr::write_volatile(
-        &mut (*port).BSRR,
-        (pin as u32) << if state { 0 } else { GPIO_NUMBER },
-    );
+    unsafe {
+        core::ptr::write_volatile(
+            &mut (*port).BSRR,
+            (pin as u32) << if state { 0 } else { GPIO_NUMBER },
+        );
+    }
 }
 
 /// GPIO read pin.
@@ -47,10 +50,11 @@ pub unsafe extern "C" fn furi_hal_gpio_write_port_pin(
 /// `gpio` must be non-null, and the memory it points to must be initialized.
 #[inline]
 pub unsafe extern "C" fn furi_hal_gpio_read(gpio: *const sys::GpioPin) -> bool {
-    let port = (*gpio).port;
-    let pin = (*gpio).pin;
+    let gpio = unsafe { *gpio };
+    let port = gpio.port;
+    let pin = gpio.pin;
 
-    furi_hal_gpio_read_port_pin(port, pin)
+    unsafe { furi_hal_gpio_read_port_pin(port, pin) }
 }
 
 /// GPIO read pin.
@@ -63,5 +67,7 @@ pub unsafe extern "C" fn furi_hal_gpio_read_port_pin(
     port: *mut sys::GPIO_TypeDef,
     pin: u16,
 ) -> bool {
-    core::ptr::read_volatile(&(*port).IDR) & pin as u32 != 0x00
+    let port = unsafe { *port };
+    let input_data_register_value = unsafe { core::ptr::read_volatile(&port.IDR) };
+    input_data_register_value & pin as u32 != 0x00
 }
