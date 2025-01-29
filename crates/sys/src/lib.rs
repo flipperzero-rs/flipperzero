a52516c9ae97ccd115d687dde3f387120179ac72
David Coles <dcoles@dcoles.net>
2025-01-29 00:14:20 -0800

    Suppress `rust_2024_compatibility` lint warnings on bindgen generated code

--- crates/sys/src/lib.rs
+++ crates/sys/src/lib.rs
@@ -28,7 +28,8 @@ mod inlines;
     clippy::missing_safety_doc,
     clippy::transmute_int_to_bool,
     clippy::useless_transmute,
-    rustdoc::broken_intra_doc_links
+    rustdoc::broken_intra_doc_links,
+    rust_2024_compatibility // temporary until bindgen target version is updated
 )]
 mod bindings;
 
